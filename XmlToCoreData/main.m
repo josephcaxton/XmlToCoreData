5ffb121fe76506f40eb3a402eb5675b1b406c83a
josephcaxton <josephcaxtonidowu@yahoo.com>
2014-02-11 09:30:53 +0000

    First Commit

---
+++ XmlToCoreData/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  XmlToCoreData
+//
+//  Created by Joseph caxton-idowu on 11/02/2014.
+//  Copyright (c) 2014 CaxtonIdowu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
